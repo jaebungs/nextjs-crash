49cd6303869f97bed3440379ff6a31f405b1a3d2
jaebungs <jaebungs@gmail.com>
2023-04-16 20:13:06 -0400

    nextjs routing and layout

---
+++ app/about/layout.jsx
@@ -0,0 +1,10 @@
+const AboutLayout = ({ children }) => {
+  return (
+    <div>
+        <h1>This is About Layout. should show in About and Team</h1>
+        {children}
+    </div>
+  )
+}
+
+export default AboutLayout
---
+++ app/about/team/page.jsx
@@ -0,0 +1,11 @@
+const TeamPage = () => {
+  return (
+    <div>
+        <h1>Team Page</h1>
+        <p>Nested routing. we are at Team page.</p>
+        <p>Create a folder and page file!</p>
+    </div>
+  )
+}
+
+export default TeamPage
