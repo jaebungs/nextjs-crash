1015581ae9ec3580c799e4b6022e2613202d219d
jaebungs <jaebungs@gmail.com>
2023-04-17 22:45:22 -0400

    suspenseBoundary and Error handling, errorBoundary

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,9 +1,26 @@
+import { Suspense } from "react"
+import Repo from "@/app/components/Repo"
+import RepoDirs from "@/app/components/RepoDirs"
+import Link from "next/link"
+import ErrorBoundary from "@/app/components/ErrorBoundary"
+
 const RepoPage = ({ params: { name } }) => {
   return (
     <div className="card">
         {/* <h2>{params.name}</h2> */}
-        <h2>{name}</h2>
-        <p>Repo Details</p>
+        <Link href="/code/repos" className="btn btn-blue">
+            Back to Repositiories
+        </Link>
+        <Suspense fallback={<div>Loading repos...</div>}>
+            <Repo name={name} />
+        </Suspense>
+
+        <Suspense fallback={<div>Loading directories...</div>}>
+            <ErrorBoundary>
+                <RepoDirs name={name} />
+            </ErrorBoundary>
+        </Suspense>
+
     </div>
   )
 }
---
+++ app/components/RepoDirs.jsx
@@ -0,0 +1,37 @@
+import Link from "next/link"
+
+async function fetchRepoContents(name) {
+    // To demonstrate suspense boundary, add delay
+    await new Promise(resolve => setTimeout(resolve, 1500))
+    let content
+    try {
+        const response = await fetch(`https://api.gitbuh.com/repos/jaebungs/${name}/contents`)
+        content = await response.json()
+    } catch(error) {
+        new Error('dir fetch failed')
+    }
+
+    return content
+}
+
+
+const RepoDirs = async (name) => {
+    const contents = await fetchRepoContents(name)
+    const dirs = contents?.filter(content => content.type === 'dir')
+
+  return (
+    <>
+        <h3>Directories</h3>
+            <ul>
+                { contents && dirs?.map( dir => {
+                    <li className="" key={ dir.path }>
+                        <Link href={`/code/repos/${name}/${dir.path}`}></Link>
+                    </li>
+                })}
+            </ul>
+
+    </>
+  )
+}
+
+export default RepoDirs
