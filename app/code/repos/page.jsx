53c82250bdb8700bc266897397c72936305b6ecf
jaebungs <jaebungs@gmail.com>
2023-04-17 21:31:47 -0400

    loading component

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -3,6 +3,8 @@ import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa'
 
 async function fetchRepos() {
     const response = await fetch('https://api.github.com/users/jaebungs/repos')
+    
+    await new Promise((resolve) => setTimeout(resolve, 1000))
     const repos = await response.json()
     
     return repos
---
+++ app/loading.jsx
@@ -0,0 +1,10 @@
+
+const loading = () => {
+  return (
+    <div className="loader">
+        <div className="spinner"></div>
+    </div>
+  )
+}
+
+export default loading
