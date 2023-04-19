fd67e377a77735a15dfae5e28f59df701b11e717
jaebungs <jaebungs@gmail.com>
2023-04-18 21:16:47 -0400

    add post request

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,7 +1,21 @@
 import courses from './data.json'
 import { NextResponse } from 'next/server'
+import { v4 as uuidv4 } from 'uuid'
 
 export async function GET(request) {
     // return NextResponse(courses) // return string
+    return NextResponse.json(courses)
+}
+
+export async function POST(request) {
+    const { title, description, link, level } = await request.json()
+    const newCourse = {
+        id: uuidv4(),
+        title,
+        description,
+        level,
+        link
+    }
+    courses.push(newCourse)
     return NextResponse.json(courses)
 }
